191d26fbf4f493c088a3bc7f22ee91873985108d
siuuu <vedwandile2025@gmail.com>
2026-07-28 19:25:42 +0530

    feat: add COnversationEntry

---
+++ Assets/Phoeltress/AI/HistoryManager.cs
@@ -0,0 +1,9 @@
+private static List<ConversationEntry> history = new List<ConversationEntry>();
+
+public static void AddEntry(ConversationEntry entry)
+
+public static List<ConversationEntry> GetHistory()
+
+public static void ClearHistory()
+
+public static int GetCount()
---
+++ Assets/Phoeltress/AI/Models/ConversationEntry.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class ConversationEntry
+{
+    public string Prompt { get; set; }
+
+    public AIResponse Response { get; set; }
+
+    public DateTime Timestamp { get; set; }
+}
