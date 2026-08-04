4f68000770299cb1e0bafa45ac2e157fe8cd0191
siuuu <vedwandile2025@gmail.com>
2026-08-04 11:42:24 +0530

    refactor(ai): move AI generation to LMStudioService

---
+++ Assets/Phoeltress/AI/Models/LMStudioRequest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LMStudioRequest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
